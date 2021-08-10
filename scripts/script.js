34c44c68684dcc1d5a41138c43c6c7d08371e414
nguyen huy <hyquaq@gmail.com>
2021-08-10 18:08:08 +0700

    v2.2

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,35 @@
 let path =
     "https://raw.githubusercontent.com/hyquaq/WebQuestion/master/data/qp1.json";
 let questions = [];
-let currentQues = "";
+let currentQues = undefined;
 let outQuestion = document.querySelector(".form-study__body .body__question");
 let outAnswers = document.querySelectorAll(
     ".form-study__body .body__answers > li"
 );
 let btnNext = document.querySelector(".form-study__body .next");
+let yourScore = 0;
+let totalScore = 0;
+let score = document.querySelector(
+    ".main .form-study__title .title__total-score "
+);
+let divShowScore = document.querySelector(".show-score");
 
-fetch(path)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        questions = data;
-    })
-    .catch(() => console.log("error"));
+function loadQuestion(path) {
+    fetch(path)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            questions = data;
+        })
+        .catch(() => console.log("error"));
+    yourScore = 0;
+    totalScore = 0;
+}
+
+function updateScore() {
+    let html = `${yourScore}/${totalScore} points`;
+    score.innerText = html;
+}
 
 outAnswers.forEach((answer) => {
     answer.addEventListener("click", (e) => {
@@ -23,9 +38,22 @@ outAnswers.forEach((answer) => {
         if (e.target.value == currentQues.correctAnswer) {
             // console.log(e.target.classList);
             e.target.classList.add("answer--right");
+            yourScore++;
+
+            // update score
+            updateScore();
         } else {
             e.target.classList.add("answer--wrong");
+
+            // show right-answer
+            console.log(outAnswers[parseInt(currentQues.correctAnswer) - 1]);
+            outAnswers[parseInt(currentQues.correctAnswer) - 1].classList.add(
+                "answer--right"
+            );
         }
+        outAnswers.forEach((answer) => {
+            answer.classList.add("disable");
+        });
         // if(e.value)
     });
 });
@@ -35,23 +63,69 @@ function clearClass() {
         answer.classList.remove("answer--right");
         answer.classList.remove("answer--wrong");
     });
+    outAnswers.forEach((answer) => {
+        answer.classList.remove("disable");
+    });
 }
 
 btnNext.addEventListener("click", () => {
     nextQuestion();
 });
 
-function nextQuestion() {
-    // clear class
-    clearClass();
+function continueStudy() {
+    divShowScore.classList.add("disabled");
+}
 
-    currentQues = questions[Math.floor(Math.random() * questions.length)];
+function nextQuestion() {
+    if (questions !== undefined && questions.length !== 0) {
+        currentQues = questions[Math.floor(Math.random() * questions.length)];
+        // clear class
+        clearClass();
+        // update score
+        totalScore++;
+        updateScore();
+        // remove question
+        questions = questions.filter((value) => {
+            if (value != currentQues) {
+                return value;
+            }
+        });
+
+        console.log(questions.length);
+    } else if (questions.length === 0) {
+        let html = `${yourScore}/${totalScore} points`;
+        if (yourScore === 0 && totalScore === 0) {
+            html = "Chọn bài bên phải kìa thấy hog dị -.- ";
+        }
+
+        if ((yourScore / totalScore) * 100 < 50) {
+            html = "kkk tạch rồi đấy";
+        }
+        if (
+            (yourScore / totalScore) * 100 >= 50 &&
+            (yourScore / totalScore) * 100 < 60
+        ) {
+            html = "Qua môn rồi đó -.-";
+        }
+        if ((yourScore / totalScore) * 100 >= 70) {
+            html = "Cũng oke á :))";
+        }
+        if ((yourScore / totalScore) * 100 >= 90) {
+            html = "Tuyệt vời yeah!! :))";
+        }
+
+        divShowScore.innerHTML = `${html}
+        <button onclick="continueStudy()">Continue</button>`;
+        divShowScore.classList.remove("disabled");
+    }
     console.log(currentQues);
 
     // out data
-    outQuestion.innerText = currentQues.question;
-    outAnswers[0].innerText = currentQues.a;
-    outAnswers[1].innerText = currentQues.b;
-    outAnswers[2].innerText = currentQues.c;
-    outAnswers[3].innerText = currentQues.d;
+    if (currentQues !== undefined) {
+        outQuestion.innerText = currentQues.question;
+        outAnswers[0].innerText = currentQues.a;
+        outAnswers[1].innerText = currentQues.b;
+        outAnswers[2].innerText = currentQues.c;
+        outAnswers[3].innerText = currentQues.d;
+    }
 }
